06d24f362d52b32f2e92146a80de4aff7d02f766
alcinos <carion.nicolas@gmail.com>
2015-05-25 16:09:03 -0600

    working mode change for Name this game

--- doc/examples/sharedLibraryInterfaceExample.cpp
+++ doc/examples/sharedLibraryInterfaceExample.cpp
@@ -114,7 +114,7 @@ int main(int argc, char** argv) {
     // take effect.)
     ale.loadROM(argv[1]);
 
-    ale.setMode(0x16);
+    ale.setMode(0x28);
 
     RAMFeatures features;
 	vector<bool> F;
