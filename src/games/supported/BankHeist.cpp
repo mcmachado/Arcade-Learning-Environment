897e6bb449be4e13fb37f46789bfd3a8f66c7c8b
alcinos <carion.nicolas@gmail.com>
2015-05-25 13:43:56 -0600

    change mode trough simulation for bank heist

--- src/games/supported/BankHeist.cpp
+++ src/games/supported/BankHeist.cpp
@@ -112,8 +112,7 @@ void BankHeistSettings::reset(System& system, StellaEnvironment& environment) {
     m_score    = 0;
     m_terminal = false;
     m_lives    = 5;
-    writeRam(&system,0,m_mode);
-    writeRam(&system,1,m_mode);
+    setMode(m_mode,system,environment);
 }
 
 
@@ -148,9 +147,13 @@ ModeVect BankHeistSettings::getAvailableModes(){
 void BankHeistSettings::setMode(mode_t m,System &system, StellaEnvironment& environment){
     if(m>=0 && m<=28 && m%4==0){
         m_mode = m;
-        //write the new mode in ram
-        writeRam(&system,0,m);
-        writeRam(&system,1,m);
+        //Read the mode we are currently in
+        unsigned char mode = readRam(&system,0);
+        //press select until the correct mode is reached
+        while(mode!=m_mode){
+            environment.pressSelect(1);
+            mode = readRam(&system,0);
+        }
         //reset the environment to apply changes.
         environment.soft_reset();
     }else{
