9e070fe7bb655e4430e1d594ec6b3db91ef23d4b
alcinos <carion.nicolas@gmail.com>
2015-05-26 10:24:07 -0600

    fixing qbert

--- src/games/supported/QBert.cpp
+++ src/games/supported/QBert.cpp
@@ -142,7 +142,7 @@ void QBertSettings::loadState(Deserializer & ser) {
 DifficultyVect QBertSettings::getAvailableDifficulties(){
     DifficultyVect diff;
     diff.push_back(0);
-    diff.push_back(2);
+    diff.push_back(1);
     return diff;
 }
 
