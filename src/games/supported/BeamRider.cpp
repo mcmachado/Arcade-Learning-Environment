b1cbc4fe8b89676e84939cce0badf31fc3fcb0f7
alcinos <carion.nicolas@gmail.com>
2015-05-26 10:21:41 -0600

    fixing beamrider

--- src/games/supported/BeamRider.cpp
+++ src/games/supported/BeamRider.cpp
@@ -143,7 +143,7 @@ ActionVect BeamRiderSettings::getStartingActions() {
 DifficultyVect BeamRiderSettings::getAvailableDifficulties(){
     DifficultyVect diff;
     diff.push_back(0);
-    diff.push_back(2);
+    diff.push_back(1);
     return diff;
     
 }
