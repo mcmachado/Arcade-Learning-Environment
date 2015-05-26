d0578dd0fcc1636692a1c2208fe134829a0b98d9
alcinos <carion.nicolas@gmail.com>
2015-05-26 10:23:36 -0600

    fixing journey escape

--- src/games/supported/JourneyEscape.cpp
+++ src/games/supported/JourneyEscape.cpp
@@ -122,6 +122,6 @@ ActionVect JourneyEscapeSettings::getStartingActions() {
 DifficultyVect JourneyEscapeSettings::getAvailableDifficulties(){
     DifficultyVect diff;
     diff.push_back(0);
-    diff.push_back(2);
+    diff.push_back(1);
     return diff;
 }
